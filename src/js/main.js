75f1be1ef518513316f590df86b4730bb9b41423
artur-hibner <artur@hiweb.pl>
2017-08-08 23:21:33 +0200

    update gulp packages, added counter to main.js

---
+++ src/js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+    $(window).scroll(startCounter);
+
+    function startCounter() {
+        var onTop = $('#counter').offset().top - window.innerHeight;
+
+        if ($(window).scrollTop() > onTop) {
+            $(window).off("scroll", startCounter);
+            $('.counter__number').each(function() {
+                var $this = $(this),
+                    countTo = $this.attr('data-count');
+                jQuery({
+                    Counter: $this.text()
+                }).animate({
+                    Counter: countTo
+                }, {
+                    duration: 3000,
+                    easing: 'swing',
+                    step: function() {
+                        $this.text(Math.ceil(this.Counter));
+                    }
+                });
+            });
+        }
+    }
+
+});
